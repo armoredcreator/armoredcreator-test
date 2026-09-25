26ba7d0c6a4a89bb93354825d584a580325e464c
ArmoredCreator <87946567+armoredcreator@users.noreply.github.com>
2026-09-25 17:23:37 -0300

    refactor: source caption from Vision V1

--- ArmoredVision/service.py
+++ ArmoredVision/service.py
@@ -9,7 +9,7 @@ from armored_core.services import VisionResult, VisionUnresolvedError
 from .modules.v1.shopee_api import ShopeeAffiliateAPI, ShopeeProductNotFoundError
 from .modules.v1.shopee_resolver import resolve_short_url
 from .modules.v2.service import CandidateDiscovery, VisionCandidateError
-from .modules.caption.generator import CaptionGenerator, CaptionGenerationError
+from .modules.v1.caption.generator import CaptionGenerator, CaptionGenerationError
 
 
 class ArmoredVision:
